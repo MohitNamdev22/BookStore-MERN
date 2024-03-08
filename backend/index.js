79e110644201c74a00e68db8032b778ed5174e36
Mohit Namdev <namdevmohit0@gmail.com>
2024-03-09 00:58:29 +0530

    added files

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { PORT } from './config.js';
+
+const app = express();
+
+app.get('/', (request, response)=>{
+    console.log(request);
+    return response.status(234).send("welcome to my app");
+});
+
+app.listen(PORT,()=>{
+    console.log(`my server ${PORT}`)
+});
+
