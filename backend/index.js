551f1ed717144a0764dea1336bb0203e761f324b
Mohit Namdev <namdevmohit0@gmail.com>
2024-03-09 19:51:21 +0530

    Home & Spinner

--- backend/index.js
+++ backend/index.js
@@ -8,12 +8,13 @@ import cors from 'cors';
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
-app.use(cors({
-    origin:'http://localhost:3000',
-    methods:['GET','POST','PUT','DELETE'],
-    allowHeaders:['Content-Type'],
-}));
+// app.use(cors({
+//     origin:'http://localhost:3000',
+//     methods:['GET','POST','PUT','DELETE'],
+//     allowHeaders:['Content-Type'],
+// }));
 
 
 app.get('/', (request, response)=>{
