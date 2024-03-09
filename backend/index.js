0aa4f63043f29fe3d6e0e95839f5c175b0447e1b
Mohit Namdev <namdevmohit0@gmail.com>
2024-03-09 16:50:44 +0530

    added CORS

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,17 @@ import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
 import { Book } from './models/bookModel.js';
 import bookRoute from './routes/bookRoute.js';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({
+    origin:'http://localhost:3000',
+    methods:['GET','POST','PUT','DELETE'],
+    allowHeaders:['Content-Type'],
+}));
 
 
 app.get('/', (request, response)=>{
