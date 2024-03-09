7a02102f557001cb4177cd8a3d05a6d3fc4bbf00
Mohit Namdev <namdevmohit0@gmail.com>
2024-03-09 10:12:17 +0530

    delete route

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-import express, { request } from 'express';
+import express, { request, response } from 'express';
 import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
 import { Book } from './models/bookModel.js';
@@ -89,6 +89,21 @@ app.put('/books/:id',async (request, response)=>{
         return response.status(200).json({message:'Book updated successfully'});
 
     } catch(error){
+        console.log(error.message);
+        response.status(500).send({message:error.message});
+    }
+});
+
+app.delete('/books/:id',async(request,response)=>{
+    try{
+        const {id} = request.params;
+        const result = await Book.findByIdAndDelete(id);
+
+        if(!result){
+            return response.status(404).json({message:'Book not found'});
+        }
+        return response.status(200).send({message:'Book deleted success'});
+    } catch (error){
         console.log(error.message);
         response.status(500).send({message:error.message});
     }
