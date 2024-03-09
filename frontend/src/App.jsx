a2070a9a8f92ba1c911557c96ac3cf93dba89243
Mohit Namdev <namdevmohit0@gmail.com>
2024-03-09 17:42:10 +0530

    added multiple routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import {Route,Routes} from 'react';
+import CreateBook from "./pages/CreateBooks";
+import DeleteBook from "./pages/DeleteBook";
+import EditBook from "./pages/EditBook";
+import Home from "./pages/Home";
+import ShowBook from "./pages/ShowBook";
 
 const App = () =>{
   return (
-    <div className="bg-blue-200" >
-      <p className="text-black font-extrabold">Book Store Application</p>
-    </div>
+   <Routes>
+    <Route path='/' element={<Home/>} />
+    <Route path='/books/create' element={<CreateBook/>} />
+    <Route path='/books/details/:id' element={<ShowBook/>} />
+    <Route path='/books/edit/:id' element={<EditBook/>} />
+    <Route path='/books/delete/:id' element={<DeleteBook/>} />
+   </Routes>
   )
 }
 
