babb82a3665872f9389e3977452416d3fa2335b0
Mohit Namdev <namdevmohit0@gmail.com>
2024-03-10 17:33:55 +0530

    added notification alert

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -4,22 +4,25 @@ import axios from 'axios';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
 import { useNavigate, useParams } from 'react-router-dom';
+import { useSnackbar } from 'notistack';
 
 const DeleteBook = ()=>{
     const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
     const {id} = useParams();
+    const {enqueueSnackbar} = useSnackbar();
     const handleDeleteBook = ( )=>{
         setLoading(true);
         axios
         .delete(`http://localhost:5555/books/${id}`)
         .then(()=>{
             setLoading(false);
+            enqueueSnackbar('Book Deleted Successfully', {variant:'success'});
             navigate('/');
         })
         .catch((error)=>{
             setLoading(false);
-            alert('An error occured');
+            enqueueSnackbar('error', {variant:'error'});
             console.log(error);
         });
     };
